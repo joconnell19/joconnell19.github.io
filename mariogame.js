30911a47d498eb6f72497dca4d385e644211ac23
joconnell19 <72159334+joconnell19@users.noreply.github.com>
2020-11-05 23:46:14 -0800

    Add files via upload

---
+++ mariogame.js
@@ -0,0 +1,36 @@
+var mario = document.getElementById("mario");
+var shell = document.getElementById("shell");
+var score = 0;
+
+function hop()
+{
+    if(mario.classList == "animate")
+    {
+      return
+    }
+    mario.classList.add("animate");
+    setTimeout(function()
+    {
+        mario.classList.remove("animate");
+    },300);
+}
+
+var checkImpact = setInterval(function()
+{
+    let marioTop = parseInt(window.getComputedStyle(mario).getPropertyValue("top"));
+
+    let shellLeft = parseInt(window.getComputedStyle(shell).getPropertyValue("left"));
+
+    if(shellLeft < 20 && shellLeft > -20 && marioTop >= 130)
+    {
+        shell.style.animation = "none";
+        alert("Game Over! You scored: " + Math.floor(score/100));      
+        score = 0;
+        shell.style.animation = "shell 1s infinite linear";
+    }
+    else
+    {
+        score++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(score/100);
+    }
+}, 10);
